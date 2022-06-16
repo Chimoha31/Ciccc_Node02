1277118849af92e254961f87dfa8f3517e5b1b1e
Chimoha31 <mokochii1108@gmail.com>
2022-06-15 23:59:14 -0700

    Done :clap:

--- 01GraphQL/index.js
+++ 01GraphQL/index.js
@@ -5,9 +5,17 @@ const PORT = process.env.PORT || 5000;
 // GraphQL package
 const { graphqlHTTP } = require("express-graphql");
 // Get types from graphql
-const { GraphQLObjectType } = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema,
+} = require("graphql");
 
-const author = [
+const authors = [
   {
     id: 1,
     name: "Chiho",
@@ -26,24 +34,28 @@ const posts = [
   {
     id: 123,
     authorId: 1,
+    trending: true,
     title: "Love Gintama",
     description: "Explaining Love of Gintama",
   },
   {
     id: 124,
     authorId: 1,
+    trending: false,
     title: "Play with Teddy",
     description: "This is a book how to play with Teddy",
   },
   {
     id: 125,
     authorId: 3,
+    trending: true,
     title: "Gaming",
     description: "How to beat a monster in the game",
   },
   {
     id: 126,
     authorId: 2,
+    trending: false,
     title: "Kawaiiness",
     description: "Show kawaii attitude to human",
   },
@@ -52,20 +64,71 @@ const posts = [
 // In graphQL, you have to create Types
 // In that case here, author type & posts type
 
+
 const PostsType = new GraphQLObjectType({
   // configrations
   name: "Posts",
   description: "Posts for users",
-  fields: () => {
-    
-  },
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLInt), //GraphQLのint
+    },
+    authorId: {
+      type: new GraphQLNonNull(GraphQLInt),
+    },
+    trending: {
+      type: new GraphQLNonNull(GraphQLBoolean),
+    },
+    title: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    description: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  }),
 });
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
+const AuthorsType = new GraphQLObjectType({
+  // configrations
+  name: "Author",
+  description: "Author of posts",
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLInt), //GraphQLのint
+    },
+    name: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  }),
+});
+
+// 1つに集約させる
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Main query for Graphql",
+  fields: () => ({
+    posts: {
+      type: new GraphQLList(PostsType),
+      description: "Lists of Posts shared by users",
+      resolve: () => posts,
+    },
+    authors: {
+      type: new GraphQLList(AuthorsType),
+      description: "Lists of authors who shared posts",
+      resolve: () => authors,
+    },
+  }),
+});
+
+// REST API: CRUD (Create, Read, Update, Delete)
+// GraphQL: Create, Update, Delete ---->>>> MUTATIONS
+const mainSchema = new GraphQLSchema({
+  query: RootQueryType,
+});
+
+app.use("/graphql", graphqlHTTP({
     graphiql: true,
-    schema: "",
+    schema: mainSchema,
   })
 );
 
