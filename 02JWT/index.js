c475ec515595abea5009a543d066251d744050d0
Chimoha31 <mokochii1108@gmail.com>
2022-06-18 23:14:39 -0700

    init

---
+++ 02JWT/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const PORT = 1000;
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserRoutes = require("./routes/user");
+const morgan = require("morgan");
+
+require("dotenv").config();
+
+const mongoURL = process.env.MONGDB_URL;
+
+// We have app.use for introducing middlewares
+app.use(express.json());
+
+// Cors middleware asks the browser to allow resource sharing from different PORT Numbers
+app.use(cors());
+
+// Morgan as a logger to see incoming requests
+app.use(morgan("dev"));
+
+mongoose.connect(mongoURL, (error) => {
+  if (error) {
+    console.error(`There was an error connecting database, ${error}`);
+  } else {
+    console.log("Succefully connected to Database");
+  }
+});
+
+app.use("/api/v1/users", UserRoutes);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server running at port ${PORT}`);
+});
+
+// I sign up in a website
+// username/email - prabh@gmail.com
+// password - helloworld
+
+// We always save password in encrypted format so that no one knows what the password is
+
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
